8a155ff5a0585ab41f32d3ccdb43d1ae02cd2ab7
Gordon Cooper <lethalmongoose@gmail.com>
2014-11-14 16:48:53 -0700

    Removed the extra compiled files

--- Intermediate/Build/Mac/PracticeEditor/Development/Practice/Practice.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_PRACTICE_H__
-#define __AUTO_PRACTICE_H__
-//Last Write: 11/14/2014 12:06:38 AM +00:00
-#include "/Users/WilliamMiller/Desktop/intangible/Source/Practice/Public/Practice.h"
-#endif//__AUTO_PRACTICE_H__
